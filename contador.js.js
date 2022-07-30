55102795941aefad91deeb92a71c0a35660bcc58
juliet@21 <julietyartes2@gmail.com>
2022-07-30 11:53:24 -0500

    practico javascript

---
+++ contador.js.js
@@ -0,0 +1,19 @@
+var btn=document.getElementById('btn'),
+caja=document.getElementById('caja'),
+contador=0;
+
+function cambio()
+
+{   if(contador==0)
+   {
+    caja.classList.add('azul');
+    contador=1;
+}
+else{ caja.classList.remove('azul');
+   contador=0;}
+
+
+
+
+}
+btn.addEventListener('click',cambio, true)
